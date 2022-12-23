42a8b10f2d66c27821ea6c63fd0cd82a6bd8f002
Dr1N <drn.exp@gmail.com>
2022-12-23 13:26:35 +0200

    init map

--- AdventOfCode2022/Solvers/Day12Solver.cs
+++ AdventOfCode2022/Solvers/Day12Solver.cs
@@ -1,20 +1,24 @@
-﻿using AdventOfCode2022.Interfaces;
+﻿using System.Diagnostics;
+using AdventOfCode2022.Interfaces;
 
 namespace AdventOfCode2022.Solvers;
 
 public class Day12Solver : ISolver
 {
-    private readonly IEnumerable<string> data;
-    private int[,] map;
+    private const int Start = -1;
+    private const int End = -2;
+    
+    private Vertex[,] map;
 
     public Day12Solver(IEnumerable<string> data)
     {
         ArgumentNullException.ThrowIfNull(data, nameof(data));
-        InitMap(data);
+        InitializeMap(data.ToList());
     }
 
     public string PartOne()
     {
+        PrintMap(map);
         return string.Empty;
     }
 
@@ -23,10 +27,49 @@ public class Day12Solver : ISolver
         return string.Empty;
     }
 
-    private void InitMap(IEnumerable<string> data)
+    private void InitializeMap(IReadOnlyList<string> data)
     {
-        
+        var width = data[0].Length;
+        var height = data.Count;
+        map = new Vertex[height, width];
+        for (var row = 0; row < height; row++)
+        {
+            for (var col = 0; col < width; col++)
+            {
+                if (data[row][col] != 'S' && data[row][col] != 'E')
+                {
+                    map[row, col] = new Vertex(row, col, data[row][col] - 'a');
+                }
+                else
+                {
+                    map[row, col] = new Vertex(row, col, data[row][col] == 'S' ? Start : End);
+                }
+            }
+        }
     }
 
-    private record Point(int X, int Y);
+    private static void PrintMap(Vertex[,] map)
+    {
+        for (var i = 0; i < map.GetLength(0); i++)
+        {
+            for (var j = 0; j < map.GetLength(1); j++)
+            {
+                switch (map[i,j].Value)
+                {
+                    case Start:
+                        Debug.Write('S');
+                        break;
+                    case End:
+                        Debug.Write('E');
+                        break;
+                    default:
+                        Debug.Write((char)(map[i,j].Value + 'a'));
+                        break;
+                }
+            }
+            Debug.WriteLine(string.Empty);
+        }
+    }
+    
+    private record Vertex(int X, int Y, int Value);
 }
